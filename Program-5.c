b85e85e372ea9f240d3d8454158420e16ed622a6
IFTHEQARAHMED <84697179+IFTHEQARAHMED@users.noreply.github.com>
2021-06-26 19:49:20 +0530

    Update Program-5.c

--- Program-5.c
+++ Program-5.c
@@ -1,40 +1,25 @@
-//INPUT -3 1 2 3
-//OUTPUT- Total number of even numbers and odd numbers in the array are 1 and 2
+//INPUT - 4 1 2 3 4
+//OUTPUT - Sum of elements=10
 
-//INSERT THE MISSING CODE 
+
 #include <stdio.h>
+ 
+#include <stdio.h>
+ 
 int main()
 {
-    int arr[10];
-    int i, num, evennum, oddnum;
-
-    // Reads size and elements in array
-    printf("Enter the number of elements and the elements");
-    scanf("%d",&num);
-    //printf("Enter %d elements in array: ", num);
-        //printf("Enter the elements");
-    for(i=0;i<num;i++)
-    {
-        scanf("%d",&arr[i]);//missing code
-    }
-
-    evennum = 0; // Assuming 0 even numbers
-    oddnum  = 0; // Assuming 0 odd numbers
-
-    for(i=0; i<num; i++)
-    {
-        /* If the current element of array is evennumber then increment evennumber count */
-        if(arr[i]%2 == 0) //missing code
-        {
-            evennum++;
-        }
-        else
-        {
-            oddnum++; // increment oddnumber count
-        }
-}
-printf("Total number of even numbers and odd numbers in the array are %d and %d",evennum,oddnum  );//missing code
-//printf("Total number of odd numbers in the array : %d\n",   );//missing code
-
-return(0);    
+   int a[10],i,sum,n;
+   printf("Enter number of elements in array and the array elements");
+   scanf("%d", &n);
+ sum=0;
+  // printf("Enter %d elements\n", n);
+ 
+   for (i = 0; i < n; i++)
+   {
+      scanf("%d", &a[i]);
+      sum=sum+a[i]; 
+   }
+ printf("Sum of elements=%d",sum );
+ 
+  return 0;
 }
